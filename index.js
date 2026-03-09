76bec2a099ea70c8648af69ecfa85e396cccdf2c
Rabiul-Sujon <alamrabiul889@gmail.com>
2026-03-09 14:10:02 +0600

    fetching done

--- index.js
+++ index.js
@@ -17,3 +17,35 @@ document.getElementById('login-btn').addEventListener('click', () => {
     }
 });
 
+// fetch function
+ async function loadIssues(query = "") {
+    const loader = document.getElementById('loader');
+    const grid = document.getElementById('issues-grid');
+    
+    loader.classList.remove('hidden'); // Show the loading spinner
+    grid.innerHTML = "";
+
+    const url = query 
+        ? `${API_BASE}/issues/search?q=${query}` 
+        : `${API_BASE}/issues`;
+
+    try {
+        const res = await fetch(url);
+        const result = await res.json();
+        const data = result.data || [];
+
+        // CRITICAL FIX: Only update the master list 'allIssues' if it's NOT a search.
+        // This keeps the full list safe so you can click "All" later.
+        if (!query) {
+            allIssues = data;
+        }
+        
+        renderCards(data); // Display what just fetched
+    } catch (err) {
+        console.error("API Error:", err);
+        grid.innerHTML = `<p class="text-center col-span-4 py-10 text-red-500">Error loading data.</p>`;
+    } finally {
+        loader.classList.add('hidden'); // Hide the spinner
+    }
+}
+
